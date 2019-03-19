25c02d8c4b1bbcdc0a966c355558a9e95542e166
Cookie McCormick <muntananuchat@gmail.com>
2019-03-18 23:09:38 -0400

    Done.

--- db/migrate/20190318211509_add_foreign_keys_to_rides.rb
+++ db/migrate/20190318211509_add_foreign_keys_to_rides.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToRides < ActiveRecord::Migration
   def change
-    add_column :rides, :taxi_id, :belongs_to
-    add_column :rides, :passenger_id, :belongs_to
+    add_column :rides, :taxi_id, :integer
+    add_column :rides, :passenger_id, :integer
   end
 end
