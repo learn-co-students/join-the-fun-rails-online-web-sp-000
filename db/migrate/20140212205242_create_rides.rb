002507672740bd6bbd81f39f8c04a629266aea34
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-28 02:10:01 +0000

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,7 +1,11 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.belongs_to :passenger
+      t.belongs_to :taxi
+
       t.timestamps null: false
     end
   end
 end
+ 
