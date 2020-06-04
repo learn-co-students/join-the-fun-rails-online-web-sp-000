eec3edbbcdf77fa191ded2975b2f927ca366d0fb
dayofgray <graysonpberger@gmail.com>
2020-06-03 22:21:35 -0400

    Revert "Done."
    This reverts commit dc250a408d616448db012185717b8698b7da1b40.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,2 @@
 class Ride < ActiveRecord::Base
-  belongs_to :passenger
-  belongs_to :taxi
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,4 +1,2 @@
 class Taxi < ActiveRecord::Base
-  has_many :rides
-  has_many :passengers, through: :rides
 end
