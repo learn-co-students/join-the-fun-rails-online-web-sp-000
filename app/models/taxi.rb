1056f457679da247e437c115540ffc6b334c8710
daniel_feleppa <dfeleppa@gmail.com>
2020-11-10 09:34:58 -0500

    Done.

--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,11 @@
 class Taxi < ActiveRecord::Base
+    has_many :rides
+    has_many :taxis, through: :rides
+    has_many :tags, through: :taxis_tags
+    has_many :passengers, through: :rides
+end
+
+class TaxisTag < ActiveRecord::Base
+    belongs_to :taxi
+    belongs_to :tag
 end
